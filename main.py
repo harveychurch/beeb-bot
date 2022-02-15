3c287d54330bf096d5f15499b9fd0e6d5fb3a0d4
Harvey Church <harvey.church@bbc.co.uk>
2022-02-15 17:35:17 +0000

    Initial Bot Development

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-print("Testing release creation")
+import discord
+import platform
+import os
+from dotenv import load_dotenv
 
-print("Release Attempt 7 --  with tag")
+
+load_dotenv() 
+client = discord.Client()
+SECRET_TOKEN = os.environ.get("SECRET_TOKEN")
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello")
+
+    if message.content.startswith('$BBC'):
+        embed = discord.Embed(title="Video")
+        await message.channel.send(embed=embed)
+
+client.run(SECRET_TOKEN)
