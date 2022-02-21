f7134b65534c1416b3b81ad188424d9b5257b9bd
Harvey Church <harvey.church@bbc.co.uk>
2022-02-21 21:28:53 +0000

    pm2 screws with the start up script

--- start_bot.sh
+++ start_bot.sh
@@ -1,6 +1,5 @@
 echo "Running Startup Script"
-pm2 stop main_bot
 git pull
 # For if there have been any changes to the pipefile/dependancies
 pipenv install
-pm2 start "pipenv run python3 main.py" --name main_bot
+
